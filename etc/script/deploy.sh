07ba9d9b3a839778d18f29ed69ec59639a8bdbcb
robin <robin@ansvia.com>
2016-10-04 18:16:44 +0700

    add deploy script

---
+++ etc/script/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+HOST=103.60.183.23
+WEB_HOME=/home/web
+TARGET_PATH=$WEB_HOME/sso/
+
+set -e
+
+function run_on_remate {
+    ssh root@$HOST $@
+}
+
+scp target/release/sso root@$HOST:$TARGET_PATH
+
+rsync -avzrhcP static root@$HOST:$TARGET_PATH
+
+rsync -avzrhcP tmpl root@$HOST:$TARGET_PATH
+
+run_on_remate chown -R www-data:www-data $WEB_HOME
