b1319cd5ac2e732f3fdd36f9415047bfe2f7cabf
robin <robin@ansvia.com>
2016-10-13 11:48:59 +0700

    bump version 0.1.1

--- etc/script/release.sh
+++ etc/script/release.sh
@@ -27,4 +27,12 @@ if [ "$generate_build_rs" == "y" ]; then
     echo "build.rs updated."
 fi
 
+echo -n "commit and tag ? [y/n] "
+read to_commit
+
+if [ "$to_commit" == "y" ]; then
+    git commit -am "bump version $next_version"
+    git tag "v$next_version"
+fi
+
 echo "done."
